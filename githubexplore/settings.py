621819ecf5000051f7d887e001528d80f0150fce
Thiago Under <atchin_@hotmail.com>
2019-05-11 17:36:02 -0300

    add dj-database-url

--- githubexplore/settings.py
+++ githubexplore/settings.py
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Heroku PostgreSQL Configuration
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
