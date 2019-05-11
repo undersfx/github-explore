a725aa1ad12ac52a8414ceb1ef1753fc528b8fba
Thiago Under <atchin_@hotmail.com>
2019-05-11 16:57:35 -0300

    add .env file

--- githubexplore/settings.py
+++ githubexplore/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7nkj11*)b(mw%a-$4-#!i-oi2wu&r)34h!pr*x13uu%r3k+)0k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['github-repoexplorer.herokuapp.com', '127.0.0.1']
 
