23affeed8b66049fe39576885b6680ee1dcb3b4b
Thiago Under <atchin_@hotmail.com>
2019-05-11 23:13:27 -0300

    admin css fix

--- githubexplore/settings.py
+++ githubexplore/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_URL = '/static/'
-STATIC_URL = '/staticfiles/'
+STATIC_URL = os.path.join(BASE_DIR, 'staticfiles/')
 
 # Heroku PostgreSQL Configuration
 import dj_database_url
