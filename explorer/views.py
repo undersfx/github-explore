887545b3e3ae2a3c196bf0dd176945ec2f241312
undersfx <atchin_@hotmail.com>
2019-05-15 17:15:33 -0300

    add save post function

--- explorer/views.py
+++ explorer/views.py
@@ -5,8 +5,11 @@ import json
 
 # Create your views here.
 def index(request, *args, **kwargs):
-    print(request.GET, args, kwargs)
-    print(request.POST, args, kwargs)
+    print('GET: {}, {}, {}'.format(request.GET, args, kwargs))
+    print('POST: {}, {}, {}'.format(request.POST, args, kwargs))
+
+    # TODO:
+    # Quando chegar uma solicitação de POST, salvar no banco de dados
 
     repo_search = request.GET.get('search')
 
@@ -23,28 +26,3 @@ def index(request, *args, **kwargs):
         context = {}
 
     return render(request, 'find_repo.html', context)
-
-""" Exemplo data['items']:
-
-[{'created_at': '2016-12-07T00:07:02Z',
-            'created_by': 'Guido van Rossum',
-            'curated': True,
-            'description': 'Python is a dynamically typed programming language '
-                           'designed by Guido van Rossum. Much like the '
-                           'programming language Ruby, Python was designed to '
-                           'be easily read by programmers. Because of its '
-                           'large following and many libraries, Python can be '
-                           'implemented and used to do anything from webpages '
-                           'to scientific research.',
-            'display_name': 'Python',
-            'featured': True,
-            'name': 'python',
-            'released': 'February 20, 1991',
-            'score': 6896.7534,
-            'short_description': 'Python is a dynamically typed programming '
-                                 'language.',
-            'updated_at': '2019-05-15T02:53:55Z'},
-            .
-            .
-            .]
-"""
